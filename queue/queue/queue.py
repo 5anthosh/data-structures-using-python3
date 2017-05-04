c202206345b44e35986da7168a98dc6758c756be
Santhosh Kumar <santhoshkumarr1096@gmail.com>
2017-05-04 14:29:59 +0530

    update queue.py
    adding private data

--- queue/queue/queue.py
+++ queue/queue/queue.py
@@ -13,30 +13,30 @@ class Queue:
     def __init__(self):
         self.head = None
         self.tail = None
-        self.size = 0  # size of queue
+        self.__size = 0  # size of queue
         self.left = 0  # 0 means deletion, 0 at left means deleting at left  side of queue
         self.right = 1  # 1 means addition, 1 at right means adding at right side of queue
 
     def append(self, data):
         temp = Node(data)
-        if self.size == 0:
+        if self.__size == 0:
             self.head = temp
             self.tail = temp
-            self.size += 1
+            self.__size += 1
         else:
             if self.right:
                 self.tail.next = temp
                 temp.prev = self.tail
-                self.size += 1
+                self.__size += 1
                 self.tail = temp
             else:
                 self.head.prev = temp
                 temp.next = self.head
                 self.head = temp
-                self.size += 1
+                self.__size += 1
 
     def __len__(self):
-        return self.size
+        return self.__size
 
     def __repr__(self):
         current = self.head
@@ -44,7 +44,7 @@ class Queue:
         c = 0
         while current:
             if c == 0:
-                if self.size == 0:
+                if self.__size == 0:
                     return string
                 else:
                     string = str(current.data)
@@ -61,7 +61,7 @@ class Queue:
         return '(' + str(self.left)+', '+str(self.right)+')'
 
     def pop(self):
-        if self.size > 0:
+        if self.__size > 0:
             if self.right:
                 data = self.head.data
                 next = self.head.next
@@ -72,7 +72,7 @@ class Queue:
                 prev = self.tail.prev
                 prev.next = None
                 self.tail = prev
-            self.size -= 1
+            self.__size -= 1
             return data
         else:
             raise IndexError("Queue is empty")
